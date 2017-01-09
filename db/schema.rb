18e51cfd24ff87f982a744744c7cef8fc147d9c6
Naoto Hori <naoto.hori@gmail.com>
2017-01-09 10:54:53 +0900

    PetsHuggy03-6

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605081959) do
+ActiveRecord::Schema.define(version: 20160607220532) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20160605081959) do
     t.string   "uid"
     t.string   "image"
     t.string   "name"
+    t.string   "phone_number"
+    t.string   "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

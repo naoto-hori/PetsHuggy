5f8228c85171ccda4eb60591b89b27feceb5811a
Naoto Hori <naoto.hori@gmail.com>
2017-01-09 10:54:47 +0900

    PetsHuggy01-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/index'#pagesコントローラーのindexアクションを実行するurlを作成
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

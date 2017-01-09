4e7515cabcc49ab472affd6b25f912f4fa6c8a6c
Naoto Hori <naoto.hori@gmail.com>
2017-01-09 10:54:57 +0900

    PetsHuggy04-3-1

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -13,6 +13,15 @@ class ListingsController < ApplicationController
   end
 
   def create
+    # パラメーターとともに現在のユーザーのリスティングを作成
+    @listing = current_user.listings.build(listing_params)
+
+    if @listing.save
+      redirect_to edit_listing_path(@listing), notice: "リスティングを作成・保存をしました"
+    else
+      redirect_to new_listing_path, notice: "リスティングを作成・保存出来ませんでした"
+    end
+
   end
 
   def edit
@@ -20,4 +29,10 @@ class ListingsController < ApplicationController
 
   def update
   end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:home_type, :pet_type, :breeding_years, :pet_size)
+  end
+
 end
